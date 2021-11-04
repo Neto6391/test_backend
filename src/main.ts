1179428f413c1d8886f238a637405738273ce4ca
Neto6391 <neto6391@gmail.com>
2021-11-04 00:38:37 -0300

    feat(initial-config): create a some configs in main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './core/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+  await app.listen(5000);
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
 }
 bootstrap();
