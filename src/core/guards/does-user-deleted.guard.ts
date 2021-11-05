f1c096a8420fd942374928d63580b1200dc7d591
Neto6391 <neto6391@gmail.com>
2021-11-05 00:18:37 -0300

    feat(user): create a guard to check if user is deleted

---
+++ src/core/guards/does-user-deleted.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable, NotFoundException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class DoesUserDeletedGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    return this.validateRequest(request);
+  }
+
+  async validateRequest(request) {
+    console.log("request", request.user, " params ", request.params.id);
+    if (request.user) {
+      if (!request.user.isDeleted) {
+        return true;
+      }
+    
+      throw new NotFoundException('Usuario nao existe'); 
+        
+    }
+    throw new NotFoundException('Usuario nao existe'); 
+  }
+}
