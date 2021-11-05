bf62ba5f4b3b4c752f2e80768b630cce07c877fc
Neto6391 <neto6391@gmail.com>
2021-11-05 00:24:20 -0300

    feat(user): import modules to app-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './core/prisma/prisma.service';
+import { PrismaModule } from './core/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
