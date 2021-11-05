d2f433b3c652a28cf87164688b33a96d52331888
Neto6391 <neto6391@gmail.com>
2021-11-05 00:19:53 -0300

    feat(user): create a guard to check if user found and check jwt token with request

---
+++ src/core/guards/does-user-found.guard.spec.ts
@@ -0,0 +1,7 @@
+import { DoesUserFoundGuard } from './does-user-found.guard';
+
+describe('DoesUserFoundGuard', () => {
+  it('should be defined', () => {
+    expect(new DoesUserFoundGuard()).toBeDefined();
+  });
+});
