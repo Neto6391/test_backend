fe04c4a050925ad10564306f42bf4f2d6ffef712
Neto6391 <neto6391@gmail.com>
2021-11-05 19:19:21 -0300

    feat(project): create project controller

---
+++ src/modules/project/project.controller.ts
@@ -0,0 +1,53 @@
+import { Body, Controller, Post, UseGuards, Request, Put, Param, Delete, Get } from '@nestjs/common';
+import { DoesUserDeletedGuard } from 'src/core/guards/does-user-deleted.guard';
+import { DoesUserEditOrDeleteProjectGuard } from 'src/core/guards/does-user-edit-or-delete-project.guard';
+import { JwtAuthGuard } from 'src/core/guards/jwt-auth.guard';
+import { CreateProjectDto } from './dto';
+
+import { ProjectService } from './project.service';
+
+
+@Controller('projects')
+export class ProjectController {
+
+    constructor(
+        private readonly projectService: ProjectService
+    ) {}
+
+    @UseGuards(JwtAuthGuard,
+        DoesUserDeletedGuard)
+    @Post()
+    async createProject(@Request() req, @Body() createProjectDto: CreateProjectDto) {
+        const userId = req.user.id;
+        return await this.projectService.create(Object.assign({}, {...createProjectDto}, { userId }));
+    }
+
+    @UseGuards(JwtAuthGuard,
+        DoesUserDeletedGuard, DoesUserEditOrDeleteProjectGuard)
+    @Put(":id")
+    async updateProject(@Param("id") projectId: string,  @Request() req, @Body() createProjectDto: CreateProjectDto) {
+        const userId = req.user.id;
+        return await this.projectService.update(Object.assign({}, {...createProjectDto}, { userId }), parseInt(projectId));
+    }
+
+    @UseGuards(JwtAuthGuard,
+        DoesUserDeletedGuard)
+    @Get()
+    async getAllProjects() {
+        return await this.projectService.readAll();
+    }
+
+    @UseGuards(JwtAuthGuard,
+        DoesUserDeletedGuard)
+    @Get(":id")
+    async getOneProject(@Param("id") projectId: string) {
+        return await this.projectService.readById(parseInt(projectId), false);
+    }
+
+    @UseGuards(JwtAuthGuard,
+        DoesUserDeletedGuard, DoesUserEditOrDeleteProjectGuard)
+    @Delete(":id")
+    async deleteProject(@Param("id") projectId: string) {
+        return await this.projectService.softDelete(parseInt(projectId));
+    }
+}
