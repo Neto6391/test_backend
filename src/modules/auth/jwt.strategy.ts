f2fbd6cf5399a46ac9273e9e7f13d18d69a50c84
Neto6391 <neto6391@gmail.com>
2021-11-05 00:21:12 -0300

    feat(user): create a jwt strategy with passport library

---
+++ src/modules/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { User } from ".prisma/client";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PassportStrategy} from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UserService } from "../user/user.service";
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private readonly userService: UserService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWTKEY,
+        });
+    }
+
+    async validate(payload: any) {
+        const data: { user: Partial<User> }  = await this.userService.readById(payload.id) as { user: Partial<User> };
+        console.log('data', data, ' payload ', payload);
+
+        if (data) {
+            return {...data.user};
+        }
+       
+        throw new NotFoundException('Usuario nao existe');
+    }
+}
