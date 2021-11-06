f3b615db1cb2ebcf44a8adbdccb66ffd360a7fa4
Neto6391 <neto6391@gmail.com>
2021-11-06 02:06:14 -0300

    refactor(document): refactor some migrations

--- prisma/migrations/20211104023801_init/migration.sql
+++ prisma/migrations/20211104023801_init/migration.sql
@@ -15,7 +15,6 @@ CREATE TABLE `Project` (
     `userId` INTEGER NOT NULL,
     `isDeleted` BOOLEAN NOT NULL DEFAULT false,
 
-    UNIQUE INDEX `Project_userId_key`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
